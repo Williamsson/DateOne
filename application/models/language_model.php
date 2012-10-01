ca186ce156e11db41fc5b75355be710943760ae3
Simon Williamsson <DenialOfFate@live.se>
2012-10-01 15:54:24 +0200

    Bugfixing
    Beeing in another country than sweden or england gave an error

--- application/models/language_model.php
+++ application/models/language_model.php
@@ -3,7 +3,12 @@
 class Language_model extends CI_Model{
 	
 	function loadLanguage(){
-	
+		/*
+		 * Calls the getLanguage function, gets a language, finds what directory
+		 * that file is located in and loads the language
+		 * @TODO: Cache the result so we don't have to look everything up all the time
+		 */
+		
 		$lang = $this->getLanguage();
 		
 		if($lang == "se"){
@@ -16,7 +21,11 @@ class Language_model extends CI_Model{
 	}
 	
 	function getLanguage(){
-		
+		/*
+		 * Checks if the user has given a 
+		 * language in the URL, if not, locate what country the user is in
+		 * and see if that language exists
+		 */
 		$lang = $this->uri->segment(1);
 		
 		if($this->uri->segment(1) === "se"){
@@ -25,6 +34,22 @@ class Language_model extends CI_Model{
 			$lang = "en";
 		}else{
 			$lang = $this->getUserCountry();
+			
+			$allLangs = $this->getLanguageList();
+			
+			$languageAvalible = false;
+			
+			foreach($allLangs as $language){
+				if($language == $lang){
+					$languageAvalible = true;
+					$lang = $language;
+				}else{
+				}
+			}
+			
+			if(!$languageAvalible){
+				$lang = "en";
+			}
 		}
 		
 		return $lang;
@@ -37,7 +62,7 @@ class Language_model extends CI_Model{
 		//Connect to the api to find what country that IP is located in
 		$response = file('http://api.hostip.info/get_html.php?ip=' . $userIP . '&position=true');
 	
-		//Do a lot of cleaning to only get the initials of the country, eg "en" or "se" for english or swedish and the full name
+		//Do a lot of cleaning to only get the initials of the country, eg "en" or "se"
 	
 		$array = explode(" ",$response[0]);
 		$initials = strtolower($array[2]);
@@ -46,9 +71,15 @@ class Language_model extends CI_Model{
 		$replace   = array("", "", "");
 		$initials = str_replace($find, $replace, $initials);
 		
+		//Ensure that we ONLY get the two first chars
 		$initials = substr($initials, 0, 2);
 		
 		return $initials;
+	}
+	
+	function getLanguageList(){
+		$languages = array('se', 'en');
+		return $languages;
 	}
 	
 }
